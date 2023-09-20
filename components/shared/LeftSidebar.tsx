11ac487c4ccfd00d25ae0e6b906a56f995808089
Paulo Cardoso <paulo.cardoso@ztrax.com.br>
2023-09-20 18:01:09 -0300

    add: LeftSidebar not finished

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,10 +1,29 @@
+'use client';
+
 import React from 'react';
+import { sidebarLinks } from '@/constants';
+import Link from 'next/link';
+import Image from 'next/image';
+import { usePathname, useRouter } from 'next/navigation';
 
 function LeftSidebar() {
+  const router = useRouter();
+  const pathName = usePathname();
+
   return (
-    <div>
-      <h1>LeftSidebar</h1>
-    </div>
+    <section className='custom-scrollbar leftsidebar'>
+      <div className='flex w-full flex-1 flex-col gap-6 px-6'>
+        {sidebarLinks.map((link) => {
+          const isActive = (pathName.includes(link.route) && link.route.length > 1) || pathName === link.route;
+          return (
+            <Link href={link.route} key={link.label} className={`leftsidebar_link ${isActive && 'bg-primary-500'}`}>
+              <Image src={link.imgURL} alt={link.label} width={20} height={20} />
+              <p className='text-light-1 max-lg:hidden'>{link.label}</p>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
   );
 }
 
